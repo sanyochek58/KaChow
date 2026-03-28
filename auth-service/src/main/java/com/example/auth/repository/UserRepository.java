2a62ee41323b28fc8d18f7d3ce2f0f5d494c252a
Alexandr Buraev <148450042+sanyochek58@users.noreply.github.com>
2026-03-28 13:37:18 +0300

    feat(auth-service): Добалены MongoRepository для взаимодействия с БД

---
+++ auth-service/src/main/java/com/example/auth/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.auth.repository;
+
+import com.example.auth.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
