1de632424c80c34eb7dbc7e5843f35442c289b00
Alexandr Buraev <148450042+sanyochek58@users.noreply.github.com>
2026-03-28 18:13:39 +0300

    feat(auth-service): Реализация контроллера

--- auth-service/src/main/java/com/example/auth/config/SecurityConfig.java
+++ auth-service/src/main/java/com/example/auth/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
     }
---
+++ auth-service/src/main/java/com/example/auth/controller/AuthController.java
@@ -0,0 +1,57 @@
+package com.example.auth.controller;
+
+import com.example.auth.dto.request.LoginRequest;
+import com.example.auth.dto.request.RefreshRequest;
+import com.example.auth.dto.request.RegisterRequest;
+import com.example.auth.dto.response.AuthResponse;
+import com.example.auth.exception.AuthException;
+import com.example.auth.service.authorize.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest registerRequest) throws AuthException {
+        AuthResponse authResponse = authService.register(registerRequest);
+        return ResponseEntity.ok(authResponse);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest loginRequest) throws AuthException {
+        AuthResponse authResponse = authService.login(loginRequest);
+        return ResponseEntity.ok(authResponse);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Map<String, String>> logout(@RequestHeader("X-User-Id") String userId) throws AuthException {
+        authService.logout(userId);
+        return ResponseEntity.ok(Map.of("message","Успешный выход !"));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refresh(@Valid @RequestBody RefreshRequest refreshRequest) throws AuthException {
+        AuthResponse authResponse = authService.refresh(refreshRequest);
+        return ResponseEntity.ok(authResponse);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<Map<String, Object>> me(
+            @RequestHeader("X-User-Id") String userId,
+            @RequestHeader("X-User-Role") String role
+    ) {
+        return ResponseEntity.ok(Map.of(
+                "userId", userId,
+                "role", role
+        ));
+    }
+}
