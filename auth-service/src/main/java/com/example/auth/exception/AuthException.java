e31b99e08fab46eb68e77e756cadc666eda4f0cd
Alexandr Buraev <148450042+sanyochek58@users.noreply.github.com>
2026-03-28 17:51:47 +0300

    Реализация бизнес-логики авторизации и исправление недочётов

---
+++ auth-service/src/main/java/com/example/auth/exception/AuthException.java
@@ -0,0 +1,10 @@
+package com.example.auth.exception;
+
+public class AuthException extends Exception {
+    public AuthException(String message) {
+        super(message);
+    }
+    public AuthException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
