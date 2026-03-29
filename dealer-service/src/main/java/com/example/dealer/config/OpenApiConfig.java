61b43c0100879867162ae3e76b6431c7baefedd4
Alexandr Buraev <148450042+sanyochek58@users.noreply.github.com>
2026-03-29 15:24:49 +0300

    feat(dealer-service): Добавлены настройки БД и конфиг OpenAPI

---
+++ dealer-service/src/main/java/com/example/dealer/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.example.dealer.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("KaChow - Dealer Service")
+                        .description("Сервис работы с автосалонами")
+                        .version("1.0.0"));
+    }
+}
